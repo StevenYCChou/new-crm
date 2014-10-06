d7868083fa8a19bacf9e94c4ee6b746174178c6d
StevenYCChou <yencheng.chou@gmail.com>
2014-10-06 01:08:24 -0400

    Add routes

--- routes.js
+++ routes.js
@@ -5,38 +5,37 @@ var ContactHistoryController = require('./controller/ContactHistoryController.js
 var express = require('express');
 var http = require('http');
 var app = express();
-app.engine('.html', require('ejs').__express);
 app.set('views', __dirname+'/views');
-app.set('view engine', 'html');
-http.createServer(app).listen(3000);
+app.set('view engine', 'ejs'); // default view engine
 
+http.createServer(app).listen(3000);
 app.get('/', function(req, res) {
   res.render('homepage.ejs');
 });
 
 // Agent Controller
 app.get('/agents', AgentController.getAll);
-// app.post('/agent', AgentController.create);
-// app.get('/agent/:agentID', AgentController.retrieve);
-// app.get('agent/:agentID/edit', AgentController.showUpdatePage);
-// app.put('/agent/:agentID', AgentController.update);
-// app.get('/agents', AgentController.getAll);
-// app.get('/agents', AgentController.getAll);
-// app.get('agent/:agentID/customer/:customerID', AgentController.showCustomer);
 
-//   'GET /agent/create': { view: 'agents/create' },
+app.get('/agent/create', AgentController.showCreatePage); 
+app.post('/agent', AgentController.create);
+
+app.get('/agent/:agentID', AgentController.retrieve);
+app.get('/agent/:agentID/edit', AgentController.showUpdatePage);
+app.put('/agent/:agentID', AgentController.update);
+
+app.get('agent/:agentID/customer/:customerID', AgentController.showCustomer);
 
 //   // Customer Controller
-//   'GET /agent/:agentID/create': 'CustomerController.showCreate',
-//   'POST /agent/:agentID': 'CustomerController.create',
-//   'GET /agent/:agentID/customer/:customerID/edit': 'CustomerController.showUpdatePage',
-//   'PUT /agent/:agentID/customer/:customerID': 'CustomerController.update',
-//   'DELETE /customer/:agentID': 'CustomerController.delete',
+app.get('/agent/:agentID/create', CustomerController.showCreate);
+app.post('/agent/:agentID', CustomerController.create);
+app.get('/agent/:agentID/customer/:customerID/edit', CustomerController.showUpdatePage);
+app.put('/agent/:agentID/customer/:customerID', CustomerController.update);
+app.delete('/customer/:agentID', CustomerController.delete);
 app.get('/customer/:customerID', CustomerController.retrieve);
-//   'GET /customer/:customerID/edit': 'CustomerController.showUpdatePage',
-//   'POST / customer/:customerID': 'CustomerController.update',
+app.get('/customer/:customerID/edit', CustomerController.showUpdatePage);
+app.post('/customer/:customerID', CustomerController.update);
 
 //   // ContactHistory Controller
 app.get('/contact_history/:contactHistoryID', ContactHistoryController.retrieve);
-//   'GET /contact_history/create': 'ContactHistoryController.showCreate',
-//   'POST /contact_history': 'ContactHistoryController.create',
+app.get('/contact_history/create', ContactHistoryController.showCreate);
+app.post('/contact_history', ContactHistoryController.create);
