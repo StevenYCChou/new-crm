6ee9f9e84fa353deff4e4a1ebeb9d4c49141ab8c
StevenYCChou <yencheng.chou@gmail.com>
2014-10-07 23:03:51 -0400

    Add configuration data and sqs client for aws-sqs

--- aws.js
+++
@@ -1,6 +0,0 @@
-var AWS = require('aws-sdk');
-
-var creds = new AWS.SharedIniFileCredentials({ profile: '~/.aws/credentials' });
-var sqs_client = new AWS.SQS({ credentials: creds });
-
-sqs_client.
