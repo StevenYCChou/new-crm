c85dde0b4a55be04719ec8f86c21a1b2f16e8377
StevenYCChou <yencheng.chou@gmail.com>
2014-10-14 20:01:39 -0400

    Add agent related crm-service

--- crm_service.js
+++ crm_service.js
@@ -1,5 +1,7 @@
 var model = require('./model.js');
 
+
+// Agent related method
 exports.addAgent = function (newAgent, callback) {
   models['Agent'].create(newAgent, callback);
 };
@@ -12,12 +14,32 @@ exports.getAgentById = function(agentId, callback) {
   models['Agent'].findById(agentId).populate('customers').exec(callback);
 };
 
+exports.updateAgentById = function(agentId, updatedInfo, callback) {
+  models['Agent'].findByIdAndUpdate(agentId, updatedInfo, callback);
+};
+
+// Customer related method
+
 exports.getCustomerById = function(customerId, callback) {
   models['Customer'].findById(customerId).populate('agent').exec(callback);
 };
 
-exports.updateAgentById = function(agentId, updatedInfo, callback) {
-  models['Agent'].findByIdAndUpdate(agentId, updatedInfo, callback);
+exports.addCustomer = function(newCustomer, callback) {
+  models['Customer'].create(newCustomer, callback);
+};
+
+exports.updateCustomerById = function(customerId, updatedInfo, callback) {
+  models['Customer'].findByIdAndUpdate(customerId, updatedInfo, callback);
+};
+
+exports.deleteCustomerById = function(customerId, callback) {
+  models['Customer'].findByIdAndRemove(customerId, callback);
+};
+
+// ContactHistory related method
+
+exports.getContactHistoryByCustomerId = function(customerId, callback) {
+  models['ContactHistory'].where('customer').equals(customerId).exec(callback);
 };
 
 exports.getContactHistoryByAgentIdAndCustomerId = function(agentId, customerId, callback) {
