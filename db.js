d53701773cb8b38e19a0adb28700f0882eeda5e8
StevenYCChou <yencheng.chou@gmail.com>
2014-10-20 01:26:26 -0400

    remove useless file

--- db.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-module.exports = new function () {
-//  console.log('Try to connect to MongoDB via Mongoose ...');
-//  mongoose.connect('mongodb://localhost/27017');
-  var db = mongoose.connection;
-  db.on('error', console.error.bind(console, 'Mongoose connection error:'));
-  return db;
-}
