0b9d9b640d00f95726dbd57948c3cf55653dcad6
StevenYCChou <yencheng.chou@gmail.com>
2014-10-21 18:09:56 -0400

    Change variable name

--- data_service/mongodb_service.js
+++ data_service/mongodb_service.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var agentSchema = require('./schemas/agent_schema.js');
 var customerSchema = require('./schemas/customer_schema.js');
 var contactRecordSchema = require('./schemas/contact_record_schema.js');
-var configuratons = require('../configs.js');
+var configs = require('../configs.js');
 
 // Define model with Schema
 mongoose.model('Agent', agentSchema);
@@ -10,7 +10,7 @@ mongoose.model('Customer', customerSchema);
 mongoose.model('ContactRecord', contactRecordSchema);
 
 console.log('Try to connect to MongoDB via Mongoose ...');
-var db_address = configuratons.mongoDb.server_address + '/' + configuratons.mongoDb.db_name;
+var db_address = configs.mongoDb.server_address + '/' + configs.mongoDb.db_name;
 var conn = mongoose.createConnection(db_address);
 conn.on('error', console.error.bind(console, 'Mongoose connection error:'));
 
