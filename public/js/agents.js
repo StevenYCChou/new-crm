435671823f2c2a64515cbe383da927a80288c64a
StevenYCChou <yencheng.chou@gmail.com>
2014-11-17 13:00:15 -0500

    query the correct API

--- public/js/agents.js
+++ public/js/agents.js
@@ -1,9 +1,9 @@
 angular.module('crmAgentApp', []).
   controller('agentController', ['$scope', '$http', '$window', function($scope, $http, $window) {
     $scope.agents = [];
-    $http.get('/api/v1.00/entities/agents.json?all=true')
+    $http.get('/api/v1.00/entities/agents?all=true')
       .success(function(data, status, headers, config) {
-         $scope.agents = data;
+         $scope.agents = data.agents;
     });
     $scope.createAgent = function() {
       $window.location.href="/agent/create";
@@ -15,7 +15,7 @@ angular.module('crmAgentApp', []).
   }]).
   controller('createAgentController', ['$scope', '$http', '$window', function($scope, $http, $window) {
     $scope.createAgentSubmit = function(agent_name, agent_phone, agent_email) {
-      $http.post('/api/agent', {name: agent_name, phone: agent_phone, email: agent_email})
+      $http.post('/api/v1.00/entities/agent', {name: agent_name, phone: agent_phone, email: agent_email})
         .success(function(data, status, headers, config) {
 	   $window.location.href="/agents";
 	});
