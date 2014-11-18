6221f97faf3622674ec637b3f139086d52bef1e2
StevenYCChou <yencheng.chou@gmail.com>
2014-11-18 13:56:47 -0500

    Use REST api in customers angular app

--- public/js/customers.js
+++ public/js/customers.js
@@ -1,30 +1,36 @@
 var customerApp = angular.module('crmCustomerApp',[]);
 customerApp.controller('showDetailController', ['$scope', '$http', '$window', '$location', function($scope, $http, $window, $location) {
   $scope.customerId = $location.absUrl().split("/")[4];
-  $http.get('/api/customer/' + $scope.customerId)
+  $http.get('/api/v1.00/entities/customers/' + $scope.customerId)
     .success(function(data, status, headers, config) {
       $scope.customer = data.customer;
-      $scope.agent = data.agent;
-      $scope.contact_history = data.contact_history;
-    });
+      $http.get('/api/v1.00/entities/agents/' + $scope.customer.agent)
+        .success(function(data, status, headers, config) {
+          $scope.agent = data.agent;
+          $http.get('/api/v1.00/entities/contact_records?q=customer=' + $scope.customerId + ',agent=' + $scope.agent._id)
+            .success(function(data, status, headers, config) {
+              $scope.contact_records = data.contact_records;
+          });
+      });
+  });
   $scope.customerViewEdit = function(customerId) {
-    $window.location.href = "/customer/" + customerId + "/edit";
+    $window.location.href = "/customers/" + customerId + "/edit";
   };
-  }]);
+}]);
 
-controller('editDetailController', ['$scope', '$http', '$window', '$location', function($scope, $http, $window, $location) {
+customerApp.controller('editDetailController', ['$scope', '$http', '$window', '$location', function($scope, $http, $window, $location) {
   $scope.customerId = $location.absUrl().split("/")[4];
-  $http.get('/api/customer/' + $scope.customerId)
+  $http.get('/api/v1.00/entities/customers/' + $scope.customerId)
     .success(function(data, status, headers, config) {
       $scope.customer = data.customer;
     });
-  $scope.customerViewEditCustomerSubmit = function(customerId, name, phone, email) {
-    $http.post('/api/customer/' + $scope.customerId, {name: name, phone: phone, email: email})
+  $scope.editCustomerSubmit = function(customerId, name, phone, email) {
+    $http.put('/api/v1.00/entities/customers/' + $scope.customerId, {name: name, phone: phone, email: email})
       .success(function(data, status, headers, config) {
-      $window.location.href="/customer/" + $scope.customerId;
+      $window.location.href="/customers/" + customerId;
     });
   };
-  $scope.customerViewEditCustomerCancel = function(customerId) {
-    $window.location.href = "/customer/" + customerId;
+  $scope.returnToCustomer = function(customerId) {
+    $window.location.href = "/customers/" + customerId;
   };
 }]);
