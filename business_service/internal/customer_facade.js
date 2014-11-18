025beb92628cabc8f296f534235ea35aece7db03
StevenYCChou <yencheng.chou@gmail.com>
2014-11-18 13:56:09 -0500

    Render only angular page in cutsomer facade

--- business_service/internal/customer_facade.js
+++ business_service/internal/customer_facade.js
@@ -5,15 +5,5 @@ exports.retrieveProfilePage = function(req, res) {
 };
 
 exports.showProfileUpdatePage = function (req, res) {
-  var customerId = req.param('customerId');
-
-  crmService.retrieveCustomerById(customerId, function (err, customer) {
-    if (err) {
-      res.status(500).send({ error: "Database Error." });
-    } else {
-      res.render('customer/update', {
-        customer: customer
-      });
-    }
-  });
+  res.render('customer/update');
 };
