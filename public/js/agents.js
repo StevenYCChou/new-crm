21b728687a13c83919191dc3479fbc4e971f1452
StevenYCChou <yencheng.chou@gmail.com>
2014-11-19 17:32:12 -0500

    Add nonce for Agent PUT method

--- public/js/agents.js
+++ public/js/agents.js
@@ -1,5 +1,34 @@
 var agentApp = angular.module('crmAgentApp', []);
-agentApp.controller('showDetailController', ['$scope', '$http', '$window', '$location', function($scope, $http, $window, $location) {
+
+agentApp.factory('uuid2', [
+  function() {
+    function s4() {
+      return Math.floor((1 + Math.random()) * 0x10000)
+                 .toString(16).substring(1);
+    };
+    return {
+      newuuid: function() {
+      // http://www.ietf.org/rfc/rfc4122.txt
+      var s = [];
+      var hexDigits = "0123456789abcdef";
+      for (var i = 0; i < 36; i++) {
+          s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+      }
+      s[14] = "4"; // bits 12-15 of the time_hi_and_version field to 0010
+      s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1); // bits 6-7 of the clock_seq_hi_and_reserved to 01
+      s[8] = s[13] = s[18] = s[23] = "-";
+      return s.join("");
+      },
+      newguid: function() {
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+               s4() + '-' + s4() + s4() + s4();
+      }
+    }
+  }
+]);
+
+agentApp.controller('showDetailController', ['$scope', '$http', '$window', '$location',
+                                             function($scope, $http, $window, $location) {
   $scope.agent = [];
   $scope.customers = [];
   $scope.routes = $location.absUrl().split("/")[4];
@@ -25,7 +54,9 @@ agentApp.controller('showDetailController', ['$scope', '$http', '$window', '$loc
   }
 }]);
 
-agentApp.controller('updateDetailController', ['$scope', '$http', '$window', '$location', function($scope, $http, $window, $location) {
+agentApp.controller('updateDetailController', ['$scope', '$http', '$window', '$location', 'uuid2',
+                                               function($scope, $http, $window, $location, uuid2) {
+  $scope.uuid = uuid2.newuuid();
   $scope.agent = [];
   $scope.routes = $location.absUrl().split("/")[4];
   $http.get('/api/v1.00/entities/agents/' + $scope.routes)
@@ -33,7 +64,12 @@ agentApp.controller('updateDetailController', ['$scope', '$http', '$window', '$l
       $scope.agent = data.agent;
     });
   $scope.editAgentSubmit = function(agent_id, agent_name, agent_phone, agent_email) {
-    $http.put('/api/v1.00/entities/agents/' + agent_id, {name: agent_name, phone: agent_phone, email: agent_email})
+    var data ={name: agent_name, phone: agent_phone, email: agent_email};
+    $http({
+      url: '/api/v1.00/entities/agents/' + agent_id,
+      method: 'PUT',
+      headers: {'nonce' : 'PUT' + JSON.stringify(data) + $scope.uuid},
+      data: data})
     .success(function(data, status, headers, config) {
         $window.location.href="/agents/" + agent_id;
        });
