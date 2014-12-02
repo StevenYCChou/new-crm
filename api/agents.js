658ef8bd0e5b779f4ebd49ea23d2641a5df35274
StevenYCChou <yencheng.chou@gmail.com>
2014-12-02 13:47:54 -0500

    Factor out mongoose helper and agents API

---
+++ api/agents.js
@@ -0,0 +1,84 @@
+var mongodbService = require('../data_service/mongodb_service.js');
+var Promise = require('promise');
+var mongooseHelper = require('./mongoose_helper.js');
+
+exports.getAgents = function (req, res) {
+  var constraints = req.constraints;
+  var subcollectionQuery, subcollectionPromise;
+  if (req.constraints.query) {
+    subcollectionQuery = mongodbService.Agent.find(constraints.query);
+  } else {
+    subcollectionQuery = mongodbService.Agent.find({});
+  }
+  if (constraints.offset) {
+    subcollectionQuery = subcollectionQuery.skip(constraints.offset);
+  }
+  if (constraints.limit) {
+    subcollectionQuery = subcollectionQuery.limit(constraints.limit);
+  }
+  if (constraints.field) {
+    var fields_obj = mongooseHelper.getMongooseFields(constraints.field);
+    subcollectionQuery = subcollectionQuery.select(fields_obj);
+  }
+
+  subcollectionPromise = Promise.resolve(subcollectionQuery.exec());
+
+  var countQuery, countPromise;
+  if (constraints.query) {
+    countQuery = mongodbService.Agent.find(constraints.query);
+  } else {
+    countQuery = mongodbService.Agent.find({});
+  }
+  countQuery = countQuery.count();
+  countPromise = Promise.resolve(countQuery.exec());
+
+  Promise.all([subcollectionPromise, countPromise])
+         .then(function(results) {
+    var subCollection = results[0];
+    var collectionSize = results[1];
+    var data = [];
+    subCollection.forEach(function(agent, idx, agents) {
+      data[idx] = agent.toJSON();
+      data[idx].link = {
+        rel: "self",
+        href: "/api/v1.00/entities/agents/"+agent._id
+      };
+    });
+    var links = mongooseHelper.constructLinks('/api/v1.00/entities/agents',
+                                              constraints.offset,
+                                              data.length,
+                                              constraints.query,
+                                              req.query.field,
+                                              collectionSize);
+    res.json({
+      _type: "agent",
+      data: data,
+      links: links
+    });
+  }, function(err) {
+    res.status(404).end();
+  });
+};
+
+exports.getAgent = function(req, res) {
+  var id = req.params.id;
+  var query = mongodbService.Agent.findOne({_id: id});
+  if (req.constraints.field) {
+    query = query.select(mongooseHelper.getMongooseFields(req.constraints.field));
+  }
+  var promise = query.exec();
+
+  promise.then(function(agent) {
+    data = agent.toJSON();
+    data.links = [{
+      rel: "self",
+      href: "/api/v1.00/entities/agents/"+agent._id
+    }];
+    res.json({
+      _type: "agent",
+      data: data
+    });
+  }, function(err) {
+    res.status(404).end();
+  });
+}
---
+++ api/mongoose_helper.js
@@ -0,0 +1,48 @@
+var Qs = require('qs');
+
+var getLink = function (endpoint, offset, limit, query, field) {
+  var link_constraints = {
+    offset: offset,
+    limit: limit,
+    q: query,
+    field: field
+  }
+  var query_string = Qs.stringify(link_constraints);
+  if (query_string === "") {
+    return endpoint;
+  } else {
+    return endpoint+'?'+Qs.stringify(link_constraints);
+  }
+};
+
+exports.constructLinks = function(endpoint, offset, limit, original_query, field, collectionSize) {
+  var query_string;
+  if (original_query) {
+    query_string = Qs.stringify(original_query, { delimiter: ',' });
+  }
+  var res = [];
+  if (limit != 0) {
+    res[res.length] = {rel: "first", href: getLink(endpoint, 0, limit, query_string, field)};
+    res[res.length] = {rel: "last", href: getLink(endpoint, collectionSize - limit, limit, query_string, field)};
+    if (offset > 0 &&
+        collectionSize != limit) {
+      var prev_offset = (offset >= limit) ? (offset - limit) : 0;
+      res[res.length] = {rel: "prev", href: getLink(endpoint, prev_offset, limit, query_string, field)};
+    }
+    if (offset >= 0 &&
+        offset + limit < collectionSize &&
+        collectionSize != limit) {
+      var next_offset = offset + limit;
+      res[res.length] = {rel: "next", href: getLink(endpoint, next_offset, limit, query_string, field)};
+    }
+  }
+  return res;
+}
+
+exports.getMongooseFields = function (fields) {
+  var fields_obj = {};
+  fields.forEach(function(field) {
+    fields_obj[field] = 1;
+  });
+  return fields_obj;
+};
