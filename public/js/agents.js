939fd465f9f7ae3fe51dc8302ff67d06e4f7c1c5
Fan Su <fs2488@columbia.edu>
2014-12-09 22:02:04 -0500

    fixed error in customer update page

--- public/js/agents.js
+++ public/js/agents.js
@@ -208,7 +208,7 @@ agentApp.controller('editCustomerDetailController', ['$scope', '$http', '$window
       });
     };
   $scope.editCustomerCancel = function(agent_id, customer_id) {
-    $window.location.href="/agents/" + $scope.agentId + "/customers/" + $scope.customer_id;
+    $window.location.href="/agents/" + $scope.agentId + "/customers/" + $scope.customerId;
   };
 }]);
 
