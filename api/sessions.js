e2b7f5ad994291dc5824c46077d92c4551cff373
StevenYCChou <yencheng.chou@gmail.com>
2014-12-11 20:17:23 -0500

    Use promise to wrap redis callback

--- api/sessions.js
+++ api/sessions.js
@@ -1,12 +1,16 @@
 var redisService = require('../data_service/redis_service.js');
 var mongodbService = require('../data_service/mongodb_service.js');
+var Promise = require('promise');
 var redisClient = redisService.getRedisClient();
 
+var prefix = 'session:';
+var hgetallPromise = Promise.denodeify(redisClient.hgetall.bind(redisClient));
+var hmsetPromise = Promise.denodeify(redisClient.hmset.bind(redisClient));
+var delPromise = Promise.denodeify(redisClient.del.bind(redisClient));
+
 exports.getSession = function(req, res) {
-  redisClient.hgetall('session:'+req.params.id, function(err, content) {
-    if (err) {
-      res.json(err);
-    } else if (content == null) {
+  hgetallPromise(prefix + req.params.id).then(function(content) {
+    if (content == null) {
       res.json({error: 'No such session yet.'});
     } else {
       res.json({
@@ -16,30 +20,23 @@ exports.getSession = function(req, res) {
           href: '/api/v1.00/entities/sessions/'+req.params.id
         }]
       });
-    }
-  });
+    }}, function(err) {
+      res.json(err);
+    });
 };
 
 exports.updateSession = function(req, res) {
-  console.log(req.body);
-  console.log(req.params.id);
-  redisClient.hmset('session:'+req.params.id, req.body, function(err, content) {
-    console.log(err);
-    console.log(content);
-    if (err) {
-      mongodbService.Response.update({nonce: req.headers.uuid}, {$set : {status: "COMPLETED", response: err}}).exec();
-    } else {
-      mongodbService.Response.update({nonce: req.headers.uuid}, {$set : {status: "COMPLETED", response: content}}).exec();
-    }
-  })
+  hmsetPromise(prefix + req.params.id, req.body).then(function(content) {
+    mongodbService.Response.update({nonce: req.headers.uuid}, {$set : {status: "COMPLETED", response: content}}).exec();
+  }, function(err) {
+    mongodbService.Response.update({nonce: req.headers.uuid}, {$set : {status: "COMPLETED", response: err}}).exec();
+  });
 };
 
 exports.removeSession = function(req, res) {
-  redisClient.del('session:'+req.params.id, function(err, content) {
-    if (err) {
-      mongodbService.Response.update({nonce: req.headers.uuid}, {$set : {status: "COMPLETED", response: err}}).exec();
-    } else {
-      mongodbService.Response.update({nonce: req.headers.uuid}, {$set : {status: "COMPLETED", response: content}}).exec();
-    }
-  })
+  delPromise(prefix + req.params.id, req.body).then(function(content) {
+    mongodbService.Response.update({nonce: req.headers.uuid}, {$set : {status: "COMPLETED", response: content}}).exec();
+  }, function(err) {
+    mongodbService.Response.update({nonce: req.headers.uuid}, {$set : {status: "COMPLETED", response: err}}).exec();
+  });
 };
