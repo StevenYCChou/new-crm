5dba6b0bb71db7943c196812bde1afc8c65c208a
StevenYCChou <yencheng.chou@gmail.com>
2014-12-14 17:02:54 -0500

    Add importShoppingCart

--- api/actions.js
+++ api/actions.js
@@ -1,9 +1,11 @@
-var redisService = require('../../data_service/redis_service.js');
-var mongodbService = require('../../data_service/mongodb_service.js');
+var redisService = require('../data_service/redis_service.js');
+var mongodbService = require('../data_service/mongodb_service.js');
 var Promise = require('promise');
 var redisClient = redisService.getRedisClient();
 
 var hgetallPromise = Promise.denodeify(redisClient.hgetall.bind(redisClient));
+var delPromise = Promise.denodeify(redisClient.del.bind(redisClient));
+
 var productPrefix = 'product_view_stats:',
     categoryPrefix = 'category_view_stats:';
 
@@ -41,3 +43,36 @@ exports.importViewStats = function(req, res) {
     });
   });
 };
+
+
+var sessionPrefix = 'session:'
+  , userPrefix = 'user:'
+  , suffix = ':shoppingCart';
+exports.importShoppingCart = function(req, res) {
+  var sessionId = req.sessionID || '';
+  var userId = req.body.userId || '';
+
+  var multi = redisClient.multi();
+  multi.hgetall(sessionPrefix + sessionId + suffix);
+  multi.hgetall(userPrefix + userId + suffix);
+  mutli.del(sessionPrefix + sessionId + suffix);
+  multi.del(userPrefix + userId + suffix);
+
+  multi.exec(function(err, replies) {
+    sessionCart = replies[0];
+    userCart = replies[1];
+    // merge.
+    for (productId in sessionCart) {
+      userCart[proudctId] = sessionCart[productId];
+    }
+    multi.hmset(sessionPrefix + sessionId + suffix, userCart);
+    multi.hmset(userPrefix + userId + suffix, userCart);
+    multi.exec(function(err, replies) {
+      if (err) {
+        mongodbService.Response.update({nonce: req.headers.uuid}, {$set : {status: "COMPLETED", response: err}}).exec();
+      } else {
+        mongodbService.Response.update({nonce: req.headers.uuid}, {$set : {status: "COMPLETED", response: content}}).exec();
+      }
+    });
+  });
+}
