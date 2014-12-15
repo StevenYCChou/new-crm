57aac17bdcd576b329d75efa2cfd441cd6905b74
StevenYCChou <yencheng.chou@gmail.com>
2014-12-14 23:46:09 -0500

    Remove small pieces of action code

--- routers/api/actions.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var actions = require('../../api/actions.js');
-
-router.put('/import_view_stats', actions.importViewStats);
-router.put('/import_shopping_cart', actions.importShoppingCart)
-
-module.exports = router;
